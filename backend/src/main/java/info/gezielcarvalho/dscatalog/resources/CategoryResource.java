1cb1cb82bdea8b2a22f8f325a8ecb8f908887111
Geziel Carvalho <geziel.natal@gmail.com>
2022-07-23 22:30:24 -0300

    H2 Database

--- backend/src/main/java/info/gezielcarvalho/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/info/gezielcarvalho/dscatalog/resources/CategoryResource.java
@@ -3,10 +3,11 @@ package info.gezielcarvalho.dscatalog.resources;
 import org.springframework.web.bind.annotation.RestController;
 
 import info.gezielcarvalho.dscatalog.entities.Category;
+import info.gezielcarvalho.dscatalog.service.CategoryService;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/api/categories")
 public class CategoryResource {
 
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
-		return ResponseEntity.ok(list);	
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);	
 	}
 }
