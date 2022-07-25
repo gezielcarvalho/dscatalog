7772f5c3b6df15bfb76256780cfae55f23e0bab9
Geziel Carvalho <geziel.natal@gmail.com>
2022-07-25 11:27:11 -0300

    Database Seeding

--- backend/src/main/java/info/gezielcarvalho/dscatalog/service/CategoryService.java
+++ backend/src/main/java/info/gezielcarvalho/dscatalog/service/CategoryService.java
@@ -2,8 +2,10 @@ package info.gezielcarvalho.dscatalog.service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import info.gezielcarvalho.dscatalog.entities.Category;
 import info.gezielcarvalho.dscatalog.repositories.CategoryRepository;
@@ -14,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
